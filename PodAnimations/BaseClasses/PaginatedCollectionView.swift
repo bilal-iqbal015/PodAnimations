9e9ac1fee09d8828feebdfd5aba9dc7f9b682918
Bilal Iqbal <syed.bilaliqbal@systemsltd.com>
2019-11-13 18:14:20 +0500

    Code Cleaning

--- PodAnimations/BaseClasses/PaginatedCollectionView.swift
+++ PodAnimations/BaseClasses/PaginatedCollectionView.swift
@@ -19,55 +19,9 @@ class PaginatedCollectionView: UICollectionViewFlowLayout {
         
         self.scrollDirection = .horizontal
         
-        let oldSize = sizeForTopInsertions
-        
-        if insertingTopCells {
-            let newSize = collectionViewContentSize
-            let xOffset = collectionView?.contentOffset.x ?? 0 + (newSize.width - oldSize.width)
-            let newOffset = CGPoint(x: xOffset, y:  collectionView?.contentOffset.y ?? 0)
-            collectionView?.contentOffset = newOffset
-        }
-        else {
-            insertingTopCells = false
-        }
-        sizeForTopInsertions = collectionViewContentSize
     }
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-       
-//        var layoutAttributes = layoutAttributesForElements(in: collectionView!.bounds)
-//
-//        if layoutAttributes?.count == 0 {
-//            return proposedContentOffset
-//        }
-//
-//        var targetIndex = layoutAttributes!.count / 2
-//
-//        let vX = velocity.x
-//
-//        if vX > 0 {
-//            targetIndex += 1
-//        }
-//        else if vX < 0 {
-//            targetIndex -= 1
-//        }
-//        else if vX == 0 {
-//            return lastPoint
-//        }
-//
-//        if targetIndex >= layoutAttributes!.count {
-//            targetIndex = layoutAttributes!.count - 1
-//        }
-//
-//        if targetIndex < 0 {
-//            targetIndex = 0
-//        }
-//
-//        let targetAttribute = layoutAttributes?[targetIndex]
-//
-//        lastPoint = CGPoint(x: targetAttribute!.center.x - (collectionView?.bounds.size.width)! * 0.5, y: proposedContentOffset.y)
-//
-//        return lastPoint
         
         if let cv = self.collectionView {
             
@@ -79,8 +33,6 @@ class PaginatedCollectionView: UICollectionViewFlowLayout {
                 
                 var candidateAttributes : UICollectionViewLayoutAttributes?
                 for attributes in attributesForVisibleCells {
-                    
-                    // == Skip comparison with non-cell items (headers and footers) == //
                     if attributes.representedElementCategory != UICollectionView.ElementCategory.cell {
                         continue
                     }
@@ -95,8 +47,7 @@ class PaginatedCollectionView: UICollectionViewFlowLayout {
                         }
                         
                     }
-                    else { // == First time in the loop == //
-                        
+                    else {
                         candidateAttributes = attributes;
                         continue;
                     }
@@ -110,7 +61,6 @@ class PaginatedCollectionView: UICollectionViewFlowLayout {
             
         }
         
-        // Fallback
         return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
         
     }
